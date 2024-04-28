947f7e494d93f6fc24f6d37d55524a8982483245
carlostobon <carlostobon@tutanota.de>
2024-04-28 11:30:43 -0500

    add rest of script

--- script_right.sh
+++ script_right.sh
@@ -1,10 +1,37 @@
 #!/bin/sh
 
-echo "Stablishing network connection..."
-systemctl enable NetworkManager.service
-systemctl start NetworkManager.service
+ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
+hwclock --systohc
+
+sed 's/#en_US.UTF-8\ UTF-8/en_US.UTF-8\ UTF-8/' > /etc/locale.gen
+sed 's/#en_US\ ISO-8859-1/en_US\ ISO-8859-1/' > /etc/locale.gen
+
+locale-gen
+
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo KEYMAP=colemak-dh > /etc/vconsole.conf
+cp -r ./colemak-dh /usr/share/kbd/keymaps/i386/
+
+read -p "Enter a hostname: " hname
+echo "$hname" > "/etc/hostname"
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+passwd
 
 echo "Creating user..."
 read -p "Enter your username: " username
 useradd -m -g users -G wheel -s /bin/bash $username
 passwd $username
+
+systemctl enable NetworkManager.service
+systemctl start NetworkManager.service
+
+git clone "https://github.com/carlostobon/dotfiles"
+mv dotfiles "/home/$username/.dotfiles"
+cd "/home/$username/.dotfiles"
+chmod a+x "./run.sh"
+./run.sh
+
+echo exec xmonad > "/home/$username/.xinitrc"
