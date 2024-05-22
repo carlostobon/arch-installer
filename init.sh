b0caa885a5412e89ee075641dcc58c7c2d677a73
carlostobon <carlostobon@tutanota.de>
2024-05-22 04:15:58 -0500

    fix installer

--- init.sh
+++ init.sh
@@ -4,4 +4,5 @@
 echo "Proceeding to install original repo packages.."
 pacstrap -K /mnt $(cat pacman.arch)
 genfstab -U /mnt > /mnt/etc/fstab
+cp runner.sh /mnt
 arch-chroot /mnt
--- runner.sh
+++ runner.sh
@@ -17,18 +17,18 @@ echo LANG=en_US.UTF-8 > /etc/locale.conf
 echo KEYMAP=colemak-dh > /etc/vconsole.conf
 cp -r ./colemak-dh /usr/share/kbd/keymaps/i386/
 
-read -p "Enter a hostname: " hname
+read -p "Enter a HOSTNAME: " hname
 echo "$hname" > "/etc/hostname"
 
 echo "setting grub..."
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "Password for root: "
+echo "Password for ROOT: "
 passwd
 
 echo "creating user..."
-read -p "Enter your username: " username
+read -p "Enter your USERNAME: " username
 useradd -m -g users -G wheel -s /bin/bash "$username"
 echo "Password for $username"
 passwd "$username"
@@ -37,14 +37,34 @@ echo "setting network manager..."
 systemctl enable NetworkManager.service
 systemctl start NetworkManager.service
 
-echo "cloning dotfiles..."
-git clone "https://github.com/carlostobon/dotfiles" "~/$username/.dotfiles"
-mkdir "~/$username/.binaries" "~/$username/.config/wallpaper" "/usr/share/fonts/ttf"
+mkdir "/usr/share/fonts/ttf" \
+      "/home/$username/.local/bin" \
+      "/home/$username/.binaries" \
+      "/home/$username/.config/wallpaper"
 
 echo "copying fonts..."
 cp "fonts/comic_mono.ttf" "/usr/share/fonts/ttf"
 cp "fonts/comic_mono_bold.ttf" "/usr/share/fonts/ttf"
-echo "exec xmonad" > "~/$username/.xinitrc"
+
+echo "setting xmonad xorg server..."
+echo "exec xmonad" > "/home/$username/.xinitrc"
+
+echo "cloning dotfiles..."
+git clone "https://github.com/carlostobon/dotfiles" "/home/$username/.dotfiles"
+
+echo "running dotfiles..."
+(cd /home/$username/.dotfiles && ./run.sh)
+chown -R ${username} /home/$username/.dotfiles
+
+echo "compiling xmonad..."
+(cd /home/$username/.xmonad && \
+      git clone https://github.com/xmonad/xmonad && \
+      git clone https://github.com/xmonad/xmonad-contrib && \
+      stack upgrade && \
+      stack init && \
+      stack install)
+chown -R ${username} /home/$username/.xmonad
+
 
 echo "Installation done! Once logged in proceed
       to compile Xmonad and run the script in dotfiles."
